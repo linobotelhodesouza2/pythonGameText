350f9b1851fd90d9d7b220f309289ef0c9cc7e15
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-06-26 09:58:53 -0300

    Add files via upload

--- jogo.py
+++ jogo.py
@@ -10,11 +10,22 @@ print('Que legal,, vi aqui que voçê {} tem {} anos!\n '.format(nome_jogador, i
 print('Agora que já te conheço posso te levar em muitas aventuras, oque deseja fazer?')
 escolha = input('SIM - quero iniciar uma aventura \nNÃO - quero ficar por aqui\n').upper()# deixa a letra em caixa alta
 
+#ESTRUTURA DE CONDIÇÃO IF ELSE
 if 'SIM' == escolha:
     print('Que bom que voçê é um aventureiro, vamos começar então!')
+
+# ESTRUTURA DE REPETIÇÃO WHILE
+    resposta = ''# variavel recebe um valor vazio
+    tentativa = 0
+    while resposta != 'DEUS':# loop enquanto a resposta nao for colocada sempre ira repetir
+        resposta = input('Qual a resposta para vida e tudo mais?: ')
+        tentativa = tentativa + 1
+    print('\nPara chegar a esse resultado voçê utilizou {} tentativas'.format(tentativa))
+    print('Parabéns, voçê acertou!')
+
 else:
     if 'NÃO' == escolha:
         print('Pena que voçê não quer se aventurar,\n GAME OVER!')
     else:
         print('Voçê digitou um comando inválido, Jogo Encerrado')#digitando um comando fora dos dois comandos sim ou nao
-        
+
