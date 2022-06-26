6c0df940ccde8f8c91c92736f7b2ed0995745dc4
Lino Souza <82716514+linobotelhodesouza2@users.noreply.github.com>
2022-06-26 09:41:06 -0300

    Add files via upload

--- jogo.py
+++ jogo.py
@@ -5,4 +5,16 @@ print('Olá, é um prazer ter voçê {} jogando o nosso jogo!'.format(nome_jogad
 
 idade_jogador = int(input('Eu gostaria de saber em que ano voçê nasceu? '))#convertendo o ano de nascimento em numero
 idade_calculada = 2022 - idade_jogador
-print('Que legal,, vi aqui que voçê {} tem {} anos!'.format(nome_jogador, idade_calculada))
+print('Que legal,, vi aqui que voçê {} tem {} anos!\n '.format(nome_jogador, idade_calculada))
+
+print('Agora que já te conheço posso te levar em muitas aventuras, oque deseja fazer?')
+escolha = input('SIM - quero iniciar uma aventura \nNÃO - quero ficar por aqui\n').upper()# deixa a letra em caixa alta
+
+if 'SIM' == escolha:
+    print('Que bom que voçê é um aventureiro, vamos começar então!')
+else:
+    if 'NÃO' == escolha:
+        print('Pena que voçê não quer se aventurar,\n GAME OVER!')
+    else:
+        print('Voçê digitou um comando inválido, Jogo Encerrado')#digitando um comando fora dos dois comandos sim ou nao
+        
